8c4bdffb011a58b9c10162afd0a4c99b69e95b3f
prajwalhassan7@gmail.com <your_email@example.com>
2024-04-19 08:29:37 +0530

    RNN PREPROCESSING UPDATED

--- DL & NN/RNN/rnn.py
+++ DL & NN/RNN/rnn.py
@@ -3,4 +3,19 @@ import numpy as np
 import matplotlib as plt
 
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = dataset_train.iloc[:,1:2].values()
+training_set = dataset_train.iloc[:,1:2].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler()
+training_set_scaled = sc.fit_transform(training_set)
+
+x_train = []
+y_train = []
+
+for i in range(60, 1257):
+    x_train.append(training_set_scaled[i-60:i,0])
+    y_train.append(training_set_scaled[i,0])
+    
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))

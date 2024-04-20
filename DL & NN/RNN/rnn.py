afc773e2f6540cf205ce4dfbd2b862fea7352945
prajwalhassan7@gmail.com <your_email@example.com>
2024-04-20 08:21:52 +0530

    TRAINED AND TESTED LSTM

--- DL & NN/RNN/rnn.py
+++ DL & NN/RNN/rnn.py
@@ -19,3 +19,62 @@ for i in range(60, 1257):
 x_train, y_train = np.array(x_train), np.array(y_train)
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import Input
+
+
+regressor = Sequential()
+
+regressor.add(Input(shape=(x_train.shape[1],1)))
+
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(1))
+
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+regressor.fit(x_train, y_train,batch_size=32 , epochs=100)
+
+dataset_test = pd.read_csv('Google_Stock_Price_Train.csv')
+real_stock_price = dataset_train.iloc[:,1:2].values
+
+dataset_total = pd.concat((dataset_train['open'],dataset_test['open']), axis=0)
+inputs = dataset_total[len(dataset_total)-len(dataset_test) - 60 :].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+
+x_test = []
+
+for i in range(60, 80):
+    x_test.append(inputs[i-60:i,0])
+    
+x_test = np.array(x_test)
+
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_stock_price = regressor.predict(x_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+
+
+
+
+
+
+
+
+

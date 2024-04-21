c784925f29b61315c128e797aaf8afb188ccdfd9
prajwalhassan7@gmail.com <your_email@example.com>
2024-04-21 12:47:30 +0530

    Completed LSTM Model

--- DL & NN/RNN/rnn.py
+++ DL & NN/RNN/rnn.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:,1:2].values
 
 from sklearn.preprocessing import MinMaxScaler
+
 sc = MinMaxScaler()
 training_set_scaled = sc.fit_transform(training_set)
 
@@ -17,15 +17,16 @@ for i in range(60, 1257):
     y_train.append(training_set_scaled[i,0])
     
 x_train, y_train = np.array(x_train), np.array(y_train)
-
+print(x_train.shape[0])
+print(x_train.shape[1])
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.layers import Input
-
 
 regressor = Sequential()
 
@@ -49,32 +50,31 @@ regressor.compile(optimizer='adam', loss='mean_squared_error')
 
 regressor.fit(x_train, y_train,batch_size=32 , epochs=100)
 
-dataset_test = pd.read_csv('Google_Stock_Price_Train.csv')
-real_stock_price = dataset_train.iloc[:,1:2].values
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
 
-dataset_total = pd.concat((dataset_train['open'],dataset_test['open']), axis=0)
-inputs = dataset_total[len(dataset_total)-len(dataset_test) - 60 :].values
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
-
-x_test = []
-
+X_test = []
 for i in range(60, 80):
-    x_test.append(inputs[i-60:i,0])
-    
-x_test = np.array(x_test)
-
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-predicted_stock_price = regressor.predict(x_test)
+    X_test.append(inputs[i- 60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 
 
+import matplotlib.pyplot as plt
 
+plt.plot(real_stock_price, color='red', label='Actual Google Stock Price')
 
+plt.plot(predicted_stock_price, color='blue', label='Predicted Google Stock Price')
 
-
-
-
-
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
